397f7b6ea6f2bd211404b741dbe34f980d29b244
Marcos Vinícius <marcosvinyson@gmail.com>
2023-07-25 21:30:36 -0300

    ex 01 02 03

---
+++ ex02.js
@@ -0,0 +1,6 @@
+
+const prompt = require ("prompt-sync")();
+
+let nome = prompt("Digite seu nome:")
+
+console.log("Olá" , nome , "prazer em conhece - lo");
---
+++ ex03.js
@@ -0,0 +1,7 @@
+const prompt = require("prompt-sync")();
+
+let n1 = parseInt(prompt("Digite um numero "));
+let n2 = parseInt(prompt("Digite um numero "));
+
+let soma = n1 + n2;
+console.log(soma)
